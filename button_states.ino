1a7c00bf0af41ce4b48747ba37e74ece9d05ecb0
IcePhoenix295 <44686324+IcePhoenix295@users.noreply.github.com>
2020-04-06 02:41:43 +0200

    Hopefully this commits this time
    What I tried to upload before...

---
+++ button_states.ino
@@ -0,0 +1,31 @@
+#include "button.h"
+
+
+
+//corresponds to the A button on our Zumo
+const uint8_t buttonPin = 14; 
+//create out button
+Button pushButton(buttonPin,15);
+
+
+
+void setup() 
+{
+  //set up serial monitor
+  Serial.begin(9600);
+  //initialize our button
+  pushButton.Init();
+}
+
+//how many times is the button pressed (first press will show count, so start at 1)
+unsigned long pushCount = 1;
+
+void loop() 
+{
+  //if pushed...
+  if(pushButton.CheckButtonPress()) 
+  {
+    //update count
+    Serial.println(pushCount++);
+  }
+}
